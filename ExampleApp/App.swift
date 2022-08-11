702c9e5e69898d00435f889e3d44ccf7ade175cf
una <unagamix@gmail.com>
2022-08-12 01:05:57 +0900

    feat: scheme for app env

--- ExampleApp/App.swift
+++ ExampleApp/App.swift
@@ -1,9 +1,13 @@
 import SwiftUI
 
+private func getEnvString() -> String {
+    return ProcessInfo.processInfo.environment["RUN_ENV"] ?? "N/A"
+}
+
 struct MainView: View {
     var body: some View {
         NavigationView {
-            Text("Hoge")
+            Text("env: \(getEnvString())")
         }
     }
 }
